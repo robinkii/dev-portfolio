952830629699ca08a126eb206df7fd4418950861
robinkii <littlerobin@bandgeek.dev>
2023-11-27 14:09:47 -0500

    Add custom themes to tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -7,4 +7,34 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+
+  daisyui: {
+    themes: [
+      {
+        myLightTheme: {
+          primary: '#ad00ff',
+          secondary: '#0028ff',
+          accent: '#76b100',
+          neutral: '#252a3f',
+          'base-100': '#fcfcfc',
+          info: '#00b1ff',
+          success: '#1fa43e',
+          warning: '#b38200',
+          error: '#ff6095',
+        },
+
+        myDarkTheme: {
+          primary: '#ad00ff',
+          secondary: '#0028ff',
+          accent: '#76b100',
+          neutral: '#fcfcfc',
+          'base-100': '#252a3f',
+          info: '#00b1ff',
+          success: '#1fa43e',
+          warning: '#b38200',
+          error: '#ff6095',
+        },
+      },
+    ],
+  },
 };
